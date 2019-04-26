11e72e1620a218a30e3fd0f39f97eec91ed26012
Bill Jang <malloc099@gmail.com>
2019-04-26 20:11:29 +0900

    "Add reviewEntry.js for db entry preset"

---
+++ app/src/components/Firebase/reviewEntry.js
@@ -0,0 +1,41 @@
+export const reviewEntry = {
+    // Time Stamp
+    "createAt": "",
+    "updateAt": "",
+
+    // Identification
+    "userId ": "",
+    "reviewId ": "",
+
+    // Basic Informatino
+    "title ": "",
+    "authors": "",
+    "publishDate ": "",
+    "published ": "",
+    "link": "",
+
+    // State
+    "toRead": "",
+    "pinned": "",
+    "trash": "",
+
+    // Tags
+    "tags": [
+        {
+            "key": "",
+            "name": "",
+        }
+    ],
+
+    // Note
+    "comment": "",
+    "boxes": [
+        {
+            "box": "",
+            "format": "",
+            "figure": "",
+            "subtitle": "",
+            "content": "",
+        }
+    ],
+}

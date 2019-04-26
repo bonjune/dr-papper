aaf2575b8f71c484c391c960022b08c025725066
Bill Jang <malloc099@gmail.com>
2019-04-27 00:14:18 +0900

    Add AddButton.js -- basic layout

---
+++ app/src/components/MenuBar/AddButton/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+import PapperEditor from "../../PapperEditor";
+
+export class AddButton extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+
+export default AddButton;

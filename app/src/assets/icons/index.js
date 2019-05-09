fa965d4c346b1fafb6086848fd38064192229325
Bill Jang <malloc099@gmail.com>
2019-05-09 20:22:26 +0900

    Icons Constants

---
+++ app/src/assets/icons/index.js
@@ -0,0 +1,27 @@
+import addReviewImg from "./Menubar_addReview.png";
+import readImg from './readIcon.png';
+import toReadImg from './toreadIcon.png';
+import pinImg from './pinIcon.png';
+import trashImg from './MenuBar_trashBin.png';
+import figureFormatImg from "./PapperEditor_figureFormat.png";
+import contentFormatImg from "./PapperEditor_contentFormat.png";
+import addBoxImg from "./PapperEditor_addBox.png";
+import userImg from "./NavBar_user.png";
+import settingImg from "./NavBar_setting.png";
+
+export const AddReviewIcon = addReviewImg;
+
+// MenuBar
+export const ReadIcon = readImg;
+export const ToReadIcon = toReadImg;
+export const PinIcon = pinImg;
+export const TrashIcon = trashImg;
+
+// PapperEditor
+export const FigureFormatIcon = figureFormatImg;
+export const ContentFormatIcon = contentFormatImg;
+export const AddBoxIcon = addBoxImg;
+
+// NavigationBar
+export const UserIcon = userImg;
+export const SettingIcon = settingImg;

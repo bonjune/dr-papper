e08c2f71a0cbf19535e38d006c4571ec66da5d57
Bill Jang <malloc099@gmail.com>
2019-05-11 21:26:47 +0900

    Add Routes Constants

--- app/src/constants/routes.js
+++ app/src/constants/routes.js
@@ -1,5 +1,7 @@
 export const HOME = "/";
-export const TO_READ = "/toread";
+export const SIGN_IN = "/signin";
+export const SIGN_UP = "/signup";
 export const READ = "/read";
+export const TO_READ = "/toread";
 export const ACCOUNT = "/account";
 export const COMPONENTS = "/components";

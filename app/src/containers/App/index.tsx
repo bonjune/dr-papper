b00802d83270879cb5beb19926e68e53a7565280
Bill Jang <malloc099@gmail.com>
2019-05-11 21:36:58 +0900

    Divide App for auth and non-auth users

--- app/src/containers/App/index.tsx
+++ app/src/containers/App/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route, withRouter } from 'react-router-dom';
 
 // Components;
 import MenuBar from "../../components/MenuBar";
@@ -10,7 +10,11 @@ import TagBar from '../../components/TagBar';
 import Board, { BoardPredicate } from '../Board';
 
 import * as ROUTES from "../../constants/routes";
-import SignIn from 'src/components/Auth/SignIn';
+import SignInPage from '../../components/Auth/SignIn';
+import { SignUpPage } from 'src/components/Auth/SignUp';
+import HomePage from '../Home';
+import { compose } from 'recompose';
+import { AuthUserContext, withAuthentication } from "../../components/Auth/Session";
 
 
 const INITIAL_STATE = {
@@ -27,67 +31,72 @@ class App extends React.Component<{}, {}> {
 
   render() {
     return (
-      <HashRouter>
-        <NavigationBar />
-          <div className="container full-width">
-            <div className="row">
-              <div className="col-md-1-5">
-              <MenuBar />
-              </div>
-              <div className="col-md-10-5">
-              <TagBar />
-              <Switch>
-                <Route
-                  exact={true}
-                  path={ROUTES.HOME}
-                  render={(props) => <Board {...props} boardPredicate={BoardPredicate.Read} />}
-                />
-                <Route
-                  path={ROUTES.TO_READ}
-                  render={(props) => <Board {...props} boardPredicate={BoardPredicate.ToRead} />}
-                />
-              </Switch>
-              </div>
-            </div>
-          </div>
-        <hr/>
-      </HashRouter>
+        <HashRouter>
+          <NavigationBar />
+          <AuthUserContext.Consumer>
+            {authUser => authUser ? <AppForAuth/> : <AppForNonAuth/>}
+          </AuthUserContext.Consumer>
+        </HashRouter>
     );
   }
 }
 
-const AppForAuth = () => (
-  <div className="app-for-auth">
-    <NavigationBar />
-      <div className="container full-width">
-        <div className="row">
-          <div className="col-md-1-5">
-          <MenuBar />
-          </div>
-          <div className="col-md-10-5">
-          <TagBar />
-          <Switch>
-            <Route
-              exact={true}
-              path={ROUTES.HOME}
-              render={(props) => <Board {...props} boardPredicate={BoardPredicate.Read} />}
-            />
-            <Route
-              path={ROUTES.TO_READ}
-              render={(props) => <Board {...props} boardPredicate={BoardPredicate.ToRead} />}
-            />
-          </Switch>
-          </div>
-        </div>
+/**
+ * App for un-authorised users
+ * 
+ * 1. Provides SignInForm (with SignUpLink)
+ * 2. Provides Non-functional Navigation Bar (implemented in NavigationBar component)
+ */
+const AppForNonAuthBase = () => (
+  <Switch>
+    <Route exact={true} path={ROUTES.HOME} component={HomePage}/>
+    <Route exact={true} path={ROUTES.SIGN_IN} component={SignInPage}/>
+    <Route exact={true} path={ROUTES.SIGN_UP} component={SignUpPage}/>
+  </Switch>
+)
+
+const AppForNonAuth = compose(
+  withRouter
+)(AppForNonAuthBase);
+
+/**
+ * App for authorised users
+ */
+const AppForAuthBase = () => (
+  <div className="container full-width">
+    <div className="row">
+      <div className="col-md-1">
+        <MenuBar />
       </div>
-    <hr/>
+      <div className="col-md-10">
+        <TagBar />
+        <Switch>
+          <Route
+            exact={true}
+            path={ROUTES.HOME}
+            render={(props) => <HomePage />}
+          />
+          <Route
+            exact={true}
+            path={ROUTES.READ}
+            render={(props) => <Board {...props} boardPredicate={BoardPredicate.Read} />}
+          />
+          <Route
+            path={ROUTES.TO_READ}
+            render={(props) => <Board {...props} boardPredicate={BoardPredicate.ToRead} />}
+          />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
+        </Switch>
+      </div>
+      <div className="col-md-1">
+      </div>
+    </div>
   </div>
 )
 
-const AppForNonAuth = () => (
-  <div className="app-for-non-auth">
-    <SignIn />
-  </div>
-)
+const AppForAuth = compose(
+  withRouter
+)(AppForAuthBase);
 
-export default App;
+export default withAuthentication(App);

6bef7bf0a3afda468aef4ba9bb2508547185c4fe
Bill Jang <malloc099@gmail.com>
2019-05-12 19:53:12 +0900

    Refactored SmallTag Component to Functional Component

--- app/src/components/Tag/index.tsx
+++ app/src/components/Tag/index.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 
-interface ISmallTagInterface {
-  tagName: string;
-}
-
-class SmallTag extends React.Component<ISmallTagInterface> {
-  constructor(props: ISmallTagInterface) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <span className="badge badge-light font-weight-normal papper-tag">#{this.props.tagName}</span>
-    )
-  }
-}
+const SmallTag = (props: { index: number; tagName: string; }) => (
+  <span
+    key={`tag-${props.index}`}
+    className="badge badge-light font-weight-normal papper-tag"
+  >
+    #{props.tagName}
+  </span>
+)
 
 export default SmallTag;

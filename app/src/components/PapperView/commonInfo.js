c8b2aa0bcaafb7918030d56a2d6863faebebc925
Bill Jang <malloc099@gmail.com>
2019-05-13 02:43:28 +0900

    List with entries of unique keys

--- app/src/components/PapperView/commonInfo.js
+++ app/src/components/PapperView/commonInfo.js
@@ -14,25 +14,39 @@ export default class CommonInfo extends React.Component{
             <div style={{background:"white", padding:"5px"}}>
                 <div className="form-control" style={{background:"#E3F2FD", border: "0px", fontWeight:"bold", fontSize:"30px", color: "black", marginBottom:"8px"}}>{this.props.title}</div>
                 <FormGroup row>
-                    <Label sm={2} size="lg" style={{textAlign:"right", fontWeight:"bold"}}>Authors</Label>
+                    <Label sm={2} size="lg" style={{ textAlign: "right", fontWeight: "bold" }}>
+                        Authors
+                    </Label>
                     <Col sm={10}>
-                        <div className="form-control-lg" style={{border: "0px", fontSize:"1.25rem", marginBottom:"8px"}}>{this.props.authors}</div>
+                        <div className="form-control-lg" style={{ border: "0px", fontSize: "1.25rem", marginBottom: "8px" }}>
+                            {this.props.authors}
+                        </div>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label sm={2} size="lg" style={{textAlign:"right", fontWeight:"bold"}}>Date</Label>
+                    <Label sm={2} size="lg" style={{ textAlign: "right", fontWeight: "bold" }}>
+                        Date
+                    </Label>
                     <Col sm={10}>
-                        <div className="form-control-lg" style={{border: "0px", fontSize:"1.25rem", marginBottom:"8px"}}>{this.props.publishDate}</div>
+                        <div className="form-control-lg" style={{ border: "0px", fontSize: "1.25rem", marginBottom: "8px" }}>
+                            {this.props.publishDate}
+                        </div>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label sm={2} size="lg" style={{textAlign:"right", fontWeight:"bold"}}>Published</Label>
+                    <Label sm={2} size="lg" style={{ textAlign: "right", fontWeight: "bold" }}>
+                        Published
+                    </Label>
                     <Col sm={10}>
-                        <div className="form-control-lg" style={{border: "0px", fontSize:"1.25rem", marginBottom:"8px"}}>{this.props.published}</div>
+                        <div className="form-control-lg" style={{ border: "0px", fontSize: "1.25rem", marginBottom: "8px" }}>
+                            {this.props.published}
+                        </div>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label sm={2} size="lg" style={{textAlign:"right", fontWeight:"bold"}}>Link</Label>
+                    <Label sm={2} size="lg" style={{ textAlign: "right", fontWeight: "bold" }}>
+                        Link
+                    </Label>
                     <Col sm={10}>
                         <div className="form-control-lg" style={{border: "0px", fontSize:"1.25rem", marginBottom:"8px"}}>
                             <a href={this.props.link}>{this.props.link}</a>
@@ -43,12 +57,14 @@ export default class CommonInfo extends React.Component{
 
             <div style={{background:"white", marginTop:"10px", padding:"5px"}}>
                 <FormGroup row>
-                    <Label sm={2} size="lg" style={{textAlign:"right", fontWeight:"bold"}}>Tags</Label>
+                    <Label sm={2} size="lg" style={{ textAlign: "right", fontWeight: "bold" }}>
+                        Tags
+                    </Label>
                     <Col sm={10}>
                         <div className="form-control-lg" style={{border: "0px", fontSize:"1.25rem", marginBottom:"8px"}}>
-                          {this.props.tags ? this.props.tags.map(tag => (
-                            <SmallTag tagName={tag} />
-                          )) : null}
+                            {this.props.tags ? this.props.tags.map((tag, i) => (
+                                <SmallTag key={`common-info-tag-${tag}`} tagName={tag} />
+                            )) : null}
                         </div>
                     </Col>
                 </FormGroup>

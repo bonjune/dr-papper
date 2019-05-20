5167907d106e1cec9f601b12fc4a59131f28c52b
TheStarkor <sk_and_mc@kaist.ac.kr>
2019-05-20 16:15:43 +0900

    Solve Conflict

---
+++ app/functions/src/index.ts
@@ -0,0 +1,28 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+
+
+const serviceAccount = require(
+    "../dr-papper-firebase-adminsdk-xjaxa-58afcfa88d.json"
+);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://dr-papper.firebaseio.com"
+})
+
+export const sanitizeTag = functions.database.ref('/tags/{tag}')
+    .onUpdate((snapshot, context) => {
+    const data = snapshot.after.val();
+    if (data) { // When the tag exists
+        // Remove a tag with null or undefined tags
+        if (data.reviews === null || data.reviews === undefined) {
+            const target = context.params.tag;
+            return admin.database().ref(`tags/${target}`).remove();
+        }
+    }
+    return new Promise<void>((resolve, reject) => null);
+})
